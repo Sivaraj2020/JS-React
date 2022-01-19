f6d35f004f41af737a32bac6b1d0e9969937f388
Sivaraj2020 <44099324+Sivaraj2020@users.noreply.github.com>
2022-01-19 20:53:53 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function Settingup(){
+  return <h4>First Component</h4>;
+}
+
+ReactDom.render(<Settingup/>,document.getElementById('root'));
